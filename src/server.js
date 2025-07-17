e628d5e9404d905ed93b6adb70646ca8f6b8ef5c
Viktor_Romaniukov <viktorrvi@email.ua>
2025-07-17 20:45:09 +0300

    fix err hw

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino';
+import pino from 'pino-http';
 import dotenv from 'dotenv';
 import { handleGetAllContacts, getContactById } from './controllers/contactsController.js';
 
@@ -9,9 +9,10 @@ dotenv.config();
 
 export function setupServer() {
   const app = express();
+    
+    app.use(pino());
   
     app.use(cors());
-    app.use(pino());
   
     app.get('/contacts', handleGetAllContacts);
     app.get('/contacts/:contactId', getContactById);
